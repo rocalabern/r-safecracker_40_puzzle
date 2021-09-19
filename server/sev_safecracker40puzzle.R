fc8884340cb71f120c29fde8a42893dc1fb32116
Roc Alabern <roc.alabern@gmail.com>
2021-09-19 10:07:36 +0200

    fix size

--- server/sev_safecracker40puzzle.R
+++ server/sev_safecracker40puzzle.R
@@ -5,7 +5,7 @@ output$puzzle <- renderPlotly({
       
   fig <- plotly_puzzle(c_Layers, v$c_angles, v$c_totals)
   
-  fig <- fig %>% layout(height = 800, width = 800)
+  # fig <- fig %>% layout(height = 800, width = 800)
   
   return (fig)
 })
