a15d622ae4305e42192c3287cb9da08c68df1b76
Roc Alabern <roc.alabern@gmail.com>
2021-09-19 09:00:34 +0200

    runapp script

--- scripts/safecracker_40.R
+++ scripts/safecracker_40.R
@@ -1,5 +1,9 @@
-rm(list=ls())
+# runApp ----
 library(shiny)
+runApp()
+
+# Test local ----
+rm(list=ls())
 library(plotly)
 
 source('tools/load_tools.R', encoding = "utf8")
@@ -11,4 +15,5 @@ fig <- plotly_puzzle(c_Layers, c_angles)
 # fig <- fig %>% layout(height = 800, width = 800)
 fig
 
-runApp()
+c_totals <- calc_totals(c_Layers, c_angles)
+c_totals
